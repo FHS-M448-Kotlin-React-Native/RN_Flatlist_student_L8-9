05667a4eee14185d656016e624a824f5c36f6314
maruyama2637 <113703160+maruyama2637@users.noreply.github.com>
2025-01-30 08:18:42 -0600

    day 2

--- flatlistApp/app/index.tsx
+++ flatlistApp/app/index.tsx
@@ -3,11 +3,11 @@ import {
   View,
   StyleSheet,
   FlatList,
-  TouchableOpacity,
 } from "react-native";
 import { useState } from "react";
-import colors from "../styles/colors";
 import defaultStyles from "../styles/defaultStyles";
+import ListItemSeparator from "@/components/ListItemSeparator";
+import ListItem from "@/components/ListItem";
 
 export default function Index() {
   const [selectedId, setSelectedId] = useState<string>("")
@@ -15,7 +15,6 @@ export default function Index() {
   type dataType = {
     id: string; // unique identifier
     title: string; // text we see in flatlist
-
   }
 
   const DATA: dataType[] = [
@@ -37,6 +36,11 @@ export default function Index() {
     console.log("selected" + item.title);
   };
 
+  const handleRowPress = (item: dataType) => {
+    setSelectedId(item.id);
+    console.log("selected" + item.title);
+  };
+
   return (
     <View style={defaultStyles.container}>
       <View style={defaultStyles.titleContainer}>
@@ -47,22 +51,13 @@ export default function Index() {
           <FlatList
             data={DATA}
             keyExtractor={(item: dataType) => item.id}
+            ItemSeparatorComponent={() => <ListItemSeparator />}
             renderItem={({item}) => (
-              <TouchableOpacity onPress={() => selectedList(item)}>
-                <View style={[styles.titleContainer, 
-                  {
-                    backgroundColor: 
-                      item.id === selectedId ? colors.primary : colors.secondary
-                  }
-                ]}>
-                  <Text style={[styles.titleText, 
-                    {
-                      color: 
-                      item.id === selectedId ? colors.text.light : colors.text.dark
-                    }
-                  ]}>{item.title}</Text>
-                </View>
-              </TouchableOpacity>
+              <ListItem 
+                item={item}
+                isSelected={item.id === selectedId}
+                onPress={handleRowPress}
+              />
             )}
           />
         </View>
@@ -74,16 +69,5 @@ export default function Index() {
 const styles = StyleSheet.create({
   flatlist: {
     alignItems: "center",
-  },
-  titleContainer: {
-    marginTop: 5,
-    width: 300,
-    borderTopLeftRadius: 15,
-    borderTopRightRadius: 15,
-    backgroundColor: "lightblue",
-  },
-  titleText: {
-    fontSize: 24,
-    padding: 10,
   },
 });
